0c8aae69bf42dfcddd8ee12e91b9ea3d4fed5028
Hodion <rodion.vinnichuk@mail.ru>
2020-09-15 23:47:49 +0300

    Добавил анимацию на сцене меню
    Не могу подкючить директорию в скрипте

---
+++ TiMB-Project/Assets/Скрипты/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Нету методов и немогу подключить директорию в using
+    }
+}
