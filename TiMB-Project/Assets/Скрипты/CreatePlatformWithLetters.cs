f8daacc30082400779331bbb8fe9a3ddd810a66c
Deaenuga <sevasvet88@gmail.com>
2020-09-15 14:12:47 +0300

    Добавил скрипты
    На спавн блоков и тестовый на передвижение персонажа для проверки камеры

---
+++ TiMB-Project/Assets/Скрипты/CreatePlatformWithLetters.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePlatformWithLetters : MonoBehaviour
+{
+    public GameObject platform;
+    public GameObject parentPlatform;
+    public string[] letters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float position = parentPlatform.transform.position.z-0.6f;
+        for(int i = 0;i<letters.Length;i++)
+        {
+            for(int j=0;j<letters[i].Length;j++)
+            {
+                Instantiate(platform, new Vector3(parentPlatform.transform.position.x, parentPlatform.transform.position.y, position),parentPlatform.transform.rotation);
+                position -= 1f;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TiMB-Project/Assets/Скрипты/Moving.cs
+++ TiMB-Project/Assets/Скрипты/Moving.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
+
+    private CharacterController characterController;
+    private Animator anim;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ButtonPressed();
+    }
+
+    void ButtonPressed(){
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 move = new Vector3(0,0,-4);
+            rb.velocity = move;
+            anim.SetTrigger("Jump");
+        }
     }
 }
