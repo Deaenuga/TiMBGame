6d76c694f93ebb4636e9a3188ce651b068472326
Hodion <rodion.vinnichuk@mail.ru>
2020-09-24 15:22:23 +0300

    Вращение персонажа героя в магазине по перемещению мыши(пальца)

--- TiMB-Project/Assets/Rotate.cs
+++ TiMB-Project/Assets/Rotate.cs
@@ -13,6 +13,15 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 1, 0));
+        //transform.Rotate(new Vector3(0, 1, 0));
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // поле зрение камеры
+        RaycastHit hit = new RaycastHit(); // хранит данные объекта с котором пересекся вектор камеры и параметры пересечения
+        if (Physics.Raycast(ray, out hit)) // выполняется если хоть один объект встретился
+        {
+            Vector3 rot = transform.eulerAngles; // запись предыдущего угла Эйлера "Rotation"
+            transform.LookAt(hit.point); // перевод в угол поворота из полученных координат при пересечении с объектом
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // установка нового угла
+        }
     }
 }
