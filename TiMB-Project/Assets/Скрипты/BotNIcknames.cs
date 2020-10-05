b4fa8e71de98998ad4e11cd278d3e9a42ae7e63e
Deaenuga <sevasvet88@gmail.com>
2020-10-05 15:58:05 +0300

    Добавил ники ботам, эффект при правильном нажатии на букву
    Раскидал файлы по папкам

---
+++ TiMB-Project/Assets/Скрипты/BotNIcknames.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotNIcknames : MonoBehaviour
+{
+
+    private TextMesh textMesh;
+    public string[] nicks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TextMesh>();
+        int nick = Random.Range(0, nicks.Length);
+        textMesh.text = nicks[nick].ToString();
+    }
+
+
+}
