f84b83d4abeba85d2c41b4617b2b77f3f51a558e
Hodion <rodion.vinnichuk@mail.ru>
2020-10-12 19:22:09 +0300

    Сцена конца уровня

---
+++ TiMB-Project/Assets/Скрипты/MenuEndLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEndLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Объекты для работы с меню победы
+    public GameObject[] hiddenObjects;
+
+    public void MenuEnd()
+    {
+        for (int i = 0; i < hiddenObjects.Length; i++)
+        {
+            hiddenObjects[i].SetActive(false);
+        }
+    }
+}
