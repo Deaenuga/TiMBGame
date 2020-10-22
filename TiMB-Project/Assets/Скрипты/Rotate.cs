5105a2edc30f39397a3d3d4ce0a02fa3607c1132
Hodion <rodion.vinnichuk@mail.ru>
2020-10-22 13:25:15 +0300

    Вращение на сцене магазина

--- TiMB-Project/Assets/Скрипты/Rotate.cs
+++ TiMB-Project/Assets/Скрипты/Rotate.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float rotationSpeed = 200f;
+    bool dragging = false;
+    Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
+    void OnMouseDrag()
+    {
+        dragging = true;
+    }
     void Update()
     {
-        //transform.Rotate(new Vector3(0, 1, 0));
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // поле зрение камеры
-        RaycastHit hit = new RaycastHit(); // хранит данные объекта с котором пересекся вектор камеры и параметры пересечения
-        if (Physics.Raycast(ray, out hit)) // выполняется если хоть один объект встретился
+        if(Input.GetMouseButtonUp(0))
         {
-            Vector3 rot = transform.eulerAngles; // запись предыдущего угла Эйлера "Rotation"
-            transform.LookAt(hit.point); // перевод в угол поворота из полученных координат при пересечении с объектом
-            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // установка нового угла
+            dragging = false;
+        }
+
+
+        //transform.Rotate(new Vector3(0, 1, 0)); авто вращение
+
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // поле зрение камеры
+        //RaycastHit hit = new RaycastHit(); // хранит данные объекта с котором пересекся вектор камеры и параметры пересечения
+        //if (Physics.Raycast(ray, out hit)) // выполняется если хоть один объект встретился
+        //{
+        //    Vector3 rot = transform.eulerAngles; // запись предыдущего угла Эйлера "Rotation"
+        //    transform.LookAt(hit.point); // перевод в угол поворота из полученных координат при пересечении с объектом
+        //    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // установка нового угла
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        if (dragging)
+        {
+            float x = Input.GetAxis("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+            //float y = Input.GetAxis("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+
+            rb.AddTorque(Vector3.down * x);
+            //rb.AddTorque(Vector3.right * y);
         }
     }
 }
