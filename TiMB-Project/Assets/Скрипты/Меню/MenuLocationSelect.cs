2ba58a67862d6fe5fc4a5fe0f8b377f604ffe2bb
Hodion <rodion.vinnichuk@mail.ru>
2021-05-11 21:25:02 +0300

    Работа со скрипатми
    Перенес код в отдельные скрипты

--- TiMB-Project/Assets/Скрипты/Меню/MenuLocationSelect.cs
+++ TiMB-Project/Assets/Скрипты/Меню/MenuLocationSelect.cs
@@ -11,20 +11,11 @@ public class MenuLocationSelect : MonoBehaviour
     public Material night;
     public Light sun;
 
-    public GameObject CanvasPeson;
-    public GameObject SkinsPerson;
-    public GameObject CanvasAchievements;
 
 
 
-    //Объекты для CanvasTraining
-    public Canvas CanvasTraining;
-    public GameObject BackTraining;
-    public VideoClip[] Video;
-    public GameObject VideoTraining;
-    public Canvas CanvasDailyBonus;
-    public Canvas CanvasMenu;
-    int count = 0;
+
+
 
 
 
@@ -62,63 +53,10 @@ public class MenuLocationSelect : MonoBehaviour
         }
 
 
-        if (PlayerPrefs.GetInt("PlayerTraining") == 1) //Если обучение пройдено
-        {
-            CanvasTraining.gameObject.SetActive(false); //отключаем canvas обучение
-            BackTraining.SetActive(false); //отключаем задний фон
-            VideoTraining.SetActive(false);
-            
-
-
-        }
-
-        if (PlayerPrefs.GetInt("PlayerTraining") == 0) //Если обучение не пройдено
-        {
-            CanvasMenu.gameObject.SetActive(false);
-            CanvasDailyBonus.gameObject.SetActive(false); //включаю бонусы
-            //убираем все видео
-            //задний план
-            //кнопки даллее и пропустить а лучше убрать только canvas
-
-            //сделать массив из видео идти по циклу и если доходим до конца то обучение пройдено
-            CanvasTraining.gameObject.SetActive(true);
-
-        }
-    }
-
-    public void Achievements()
-    {
-        CanvasPeson.gameObject.SetActive(false);
-        SkinsPerson.gameObject.SetActive(false);
-        CanvasAchievements.gameObject.SetActive(true);
-    }
-
-    public void SkipTraining()
-    {
         
-        PlayerPrefs.SetInt("PlayerTraining", 1); //Ставим значение что обучение было якобы пройдено
-
-        CanvasTraining.gameObject.SetActive(false);
-        VideoTraining.SetActive(false);
-        BackTraining.SetActive(false); //отключаем задний фон
-        CanvasDailyBonus.gameObject.SetActive(true); //включаю бонусы
-
-
     }
 
-    public void ContinueTrainingVideo()
-    {
-        count++;
 
-        for (int i = 0; i < Video.Length; i++)
-        {
-            if(count == 3)
-            {
-                SkipTraining();
-            }
-            VideoTraining.GetComponent<VideoPlayer>().clip = Video[count];
-        }
-    }
 
 
 
